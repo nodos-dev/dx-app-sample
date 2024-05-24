5093053e19350b10a5b09a7fe248ff9845d774f5
M. Samil Atesoglu <samil.atesoglu@nodos.dev>
2024-05-24 18:36:50 +0300

    WIP: Hello triangle

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -23,6 +23,21 @@ using Microsoft::WRL::ComPtr;
 #include <nosFlatBuffersCommon.h>
 #include <Nodos/AppAPI.h>
 
+inline void Must(bool cond, const char* errMsg = "Unspecified")
+
+{
+	if (cond)
+		return;
+	std::cerr << "Error: " << errMsg << std::endl;
+	std::cerr << "Details: " << GetLastError() << std::endl;
+	exit(1);
+}
+
+inline void Must(HRESULT res, const char* errMsg = "Unspecified")
+{
+	return Must(S_OK == res, errMsg);
+}
+
 using namespace DirectX;
 using Matrix4x4 = DirectX::XMMATRIX;
 using Vector4 = DirectX::XMVECTOR;
@@ -61,16 +76,99 @@ struct SampleEventDelegates : nos::app::IEventDelegates
 	void OnExecuteStart(nos::app::AppExecuteStart const* appExecuteStart) override {}
 };
 
-
-inline void Must(bool cond, const char* errMsg = "Error")
-
+struct HelloTriangle
 {
-	if (cond)
-		return;
-	std::cerr << errMsg << std::endl;
-	std::cerr << "Last Error: " << GetLastError() << std::endl;
-	exit(1);
-}
+	struct {
+		int Width = 1280;
+		int Height = 720;
+		HWND Handle = nullptr;
+	} Window;
+	ComPtr<ID3D12Device2> Device = nullptr;
+	ComPtr<ID3D12CommandAllocator> CmdAllocator = nullptr;
+	ComPtr<ID3D12CommandQueue> CmdQueue = nullptr;
+
+	ComPtr<IDXGISwapChain3> SwapChain = nullptr;
+	HANDLE SwapChainWaitableObject = nullptr;
+	static constexpr int BACK_BUFFER_COUNT = 3;
+	ComPtr<ID3D12Resource> SwapChainRTResources[BACK_BUFFER_COUNT] = {};
+
+	ComPtr<ID3D12DescriptorHeap> InputTexturesHeap = nullptr;
+	ComPtr<ID3D12DescriptorHeap> InputTextureSamplersHeap = nullptr;
+	ComPtr<ID3D12DescriptorHeap> RTVHeap = nullptr;
+
+	HelloTriangle(HWND windowHandle, int width, int height) : Window{ width, height, windowHandle }
+	{
+		Must(D3D12CreateDevice(nullptr, D3D_FEATURE_LEVEL_12_0, IID_PPV_ARGS(&Device)), "Unable to create D3D12 Device");
+
+		Must(Device->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, __uuidof(ID3D12CommandAllocator), (void**)&CmdAllocator), "Unable to create CommandAllocator");
+		
+		D3D12_COMMAND_QUEUE_DESC commandQueueDesc = {};
+		commandQueueDesc.Type = D3D12_COMMAND_LIST_TYPE_DIRECT;
+		Must(Device->CreateCommandQueue(&commandQueueDesc, __uuidof(ID3D12CommandQueue), (void**)&CmdQueue), "Unable to create CommandQueue");
+
+		// Create Descriptor Heap for Render Target Views and two imported
+		D3D12_DESCRIPTOR_HEAP_DESC shaderRtViewHeapDesc = {};
+		shaderRtViewHeapDesc.NumDescriptors = 10;
+		shaderRtViewHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV;
+		shaderRtViewHeapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_SHADER_VISIBLE;
+		Must(Device->CreateDescriptorHeap(&shaderRtViewHeapDesc, IID_PPV_ARGS(&InputTexturesHeap)), "Unable to create DescriptorHeap");
+
+		D3D12_DESCRIPTOR_HEAP_DESC rtvHeapDesc = {};
+		rtvHeapDesc.NumDescriptors = 10;
+		rtvHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV;
+		rtvHeapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_SHADER_VISIBLE;
+		Must(Device->CreateDescriptorHeap(&rtvHeapDesc, IID_PPV_ARGS(&RTVHeap)), "Unable to create DescriptorHeap");
+
+		D3D12_DESCRIPTOR_HEAP_DESC samplerHeapDesc = {};
+		samplerHeapDesc.NumDescriptors = 10;
+		samplerHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_SAMPLER;
+		samplerHeapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_SHADER_VISIBLE;
+		Must(Device->CreateDescriptorHeap(&samplerHeapDesc, IID_PPV_ARGS(&InputTextureSamplersHeap)), "Unable to create Sampler DescriptorHeap");
+
+		SetupSwapChain();
+		SetupPipeline();
+	}
+
+	void SetupSwapChain()
+	{
+		// Get the DXGI factory used to create the swap chain.
+		IDXGIFactory2* dxgiFactory = nullptr;
+		Must(CreateDXGIFactory2(0, __uuidof(IDXGIFactory2), (void**)&dxgiFactory), "Unable to create DXGIFactory2");
+		DXGI_SWAP_CHAIN_DESC1 sd{};
+		sd.BufferCount = BACK_BUFFER_COUNT;
+		sd.Width = Window.Width;
+		sd.Height = Window.Height;
+		sd.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+		sd.Flags = DXGI_SWAP_CHAIN_FLAG_FRAME_LATENCY_WAITABLE_OBJECT;
+		sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+		sd.SampleDesc.Count = 1;
+		sd.SampleDesc.Quality = 0;
+		sd.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
+		sd.AlphaMode = DXGI_ALPHA_MODE_UNSPECIFIED;
+		sd.Scaling = DXGI_SCALING_STRETCH;
+		sd.Stereo = FALSE;
+		ComPtr<IDXGIFactory4> dxgiFactory = nullptr;
+		ComPtr<IDXGISwapChain1> swapChain1 = nullptr;
+		Must(CreateDXGIFactory1(IID_PPV_ARGS(&dxgiFactory)));
+		Must(dxgiFactory->CreateSwapChainForHwnd(CmdQueue.Get(), Window.Handle, &sd, nullptr, nullptr, &swapChain1));
+		Must(swapChain1->QueryInterface(IID_PPV_ARGS(&SwapChain)));
+		SwapChain->SetMaximumFrameLatency(3);
+		SwapChainWaitableObject = SwapChain->GetFrameLatencyWaitableObject();
+
+		auto rtvHandle = RTVHeap->GetCPUDescriptorHandleForHeapStart();
+		for (int i = 0; i < BACK_BUFFER_COUNT; i++)
+		{
+			Must(SwapChain->GetBuffer(i, IID_PPV_ARGS(&SwapChainRTResources[i])));
+			Device->CreateRenderTargetView(SwapChainRTResources[i].Get(), nullptr, rtvHandle);
+			rtvHandle.ptr += Device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
+		}
+	}
+
+	void SetupPipeline()
+	{
+	
+	}
+};
 
 int main() {
 	SDL_WindowFlags window_flags =
@@ -95,44 +193,20 @@ int main() {
 	windowHandle = wm_info.info.win.window;
 
 	ComPtr<ID3D12Device2> device = nullptr;
-	Must(S_OK == D3D12CreateDevice(nullptr, D3D_FEATURE_LEVEL_12_0, IID_PPV_ARGS(&device)), "Unable to create D3D12 Device");
+	Must(D3D12CreateDevice(nullptr, D3D_FEATURE_LEVEL_12_0, IID_PPV_ARGS(&device)), "Unable to create D3D12 Device");
 
-	D3D12_FEATURE_DATA_D3D12_OPTIONS options;
-	Must(S_OK == device->CheckFeatureSupport(D3D12_FEATURE_D3D12_OPTIONS, reinterpret_cast<void*>(&options), sizeof(options)), "D3D12_FEATURE_D3D12_OPTIONS is not supported");
+	D3D12_FEATURE_DATA_D3D12_OPTIONS options{};
+	Must(device->CheckFeatureSupport(D3D12_FEATURE_D3D12_OPTIONS, reinterpret_cast<void*>(&options), sizeof(options)), "D3D12_FEATURE_D3D12_OPTIONS is not supported");
 
-	ComPtr<ID3D12CommandAllocator> cmdAllocator;
-	Must(S_OK == device->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, __uuidof(ID3D12CommandAllocator), (void**)&cmdAllocator), "Unable to create CommandAllocator");
+	ComPtr<ID3D12CommandAllocator> cmdAllocator = nullptr;
+	Must(device->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, __uuidof(ID3D12CommandAllocator), (void**)&cmdAllocator), "Unable to create CommandAllocator");
+
+	ComPtr<ID3D12CommandQueue> cmdQueue = nullptr;
 	D3D12_COMMAND_QUEUE_DESC commandQueueDesc = {};
 	commandQueueDesc.Type = D3D12_COMMAND_LIST_TYPE_DIRECT;
-	ComPtr<ID3D12CommandQueue> cmdQueue;
-	Must(S_OK == device->CreateCommandQueue(&commandQueueDesc, __uuidof(ID3D12CommandQueue), (void**)&cmdQueue), "Unable to create CommandQueue");
+	Must(device->CreateCommandQueue(&commandQueueDesc, __uuidof(ID3D12CommandQueue), (void**)&cmdQueue), "Unable to create CommandQueue");
 
-	// Get the DXGI factory used to create the swap chain.
-	IDXGIFactory2* dxgiFactory = nullptr;
-	Must(S_OK == CreateDXGIFactory2(0, __uuidof(IDXGIFactory2), (void**)&dxgiFactory), "Unable to create DXGIFactory2");
-
-	// Create the swap chain using the command queue, NOT using the device.  Thanks Killeak!
-	ComPtr<IDXGISwapChain3> swapChain;
-	DXGI_SWAP_CHAIN_DESC swapChainDesc{};
-	swapChainDesc.BufferCount = 3;
-	swapChainDesc.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
-	swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
-	swapChainDesc.OutputWindow = windowHandle;
-	swapChainDesc.SampleDesc.Count = 1;
-	swapChainDesc.Windowed = TRUE;
-	swapChainDesc.Flags = 0; //DXGI_SWAP_CHAIN_FLAG_FRAME_LATENCY_WAITABLE_OBJECT;
-	swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_SEQUENTIAL;
-	Must(S_OK == dxgiFactory->CreateSwapChain(cmdQueue.Get(), &swapChainDesc, (IDXGISwapChain**)swapChain.GetAddressOf()), "Unable to create Swapchain");
-
-	//increase max frame latency when required
-	if (swapChainDesc.Flags & DXGI_SWAP_CHAIN_FLAG_FRAME_LATENCY_WAITABLE_OBJECT)
-	{
-		swapChain->SetMaximumFrameLatency(3);
-	}
-
-	dxgiFactory->Release();
-
-
+	
 
 	// Initialize Nodos SDK
 	nos::app::FN_CheckSDKCompatibility* pfnCheckSDKCompatibility = nullptr;
